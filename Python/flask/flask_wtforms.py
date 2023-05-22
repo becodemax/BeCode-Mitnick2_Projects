7bfa51171309553463d74958162999694bbca07e
Max Brignoli <brignolimax1@gmail.com>
2023-05-22 16:57:42 +0200

    may22-16:57

--- Python/flask/flask_wtforms.py
+++ Python/flask/flask_wtforms.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, FieldList, FormField, validators, RadioField, SelectField
-from wtforms.validators import DataRequired, InputRequired
-import pycountry
+from wtforms import StringField, SubmitField, EmailField, RadioField, SelectMultipleField, widgets, TextAreaField
+from wtforms.validators import DataRequired, StopValidation, Length
 
 # Create Flask Instance
 app = Flask(__name__)
@@ -13,11 +12,28 @@ app.config['SECRET_KEY'] = "secretkey"
 def index():
     return "Hello World!"
 
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+class MultiCheckboxAtLeastOne():
+    def __init__(self, message=None):
+        if not message:
+            message = 'At least one option must be selected.'
+        self.message = message
+
+    def __call__(self, form, field):
+        if len(field.data) == 0:
+            raise StopValidation(self.message)
+
 # Create Form Class
 class NamerForm(FlaskForm):
-    name = StringField("Prénom : ", validators=[DataRequired()])
-    lastname = StringField("Nom : ", validators=[DataRequired()])
-    country = SelectField("Pays : ", validators=[InputRequired()])
+    name = StringField("Prénom: ", validators=[DataRequired()])
+    lastname = StringField("Nom: ", validators=[DataRequired()])
+    email = EmailField('Email: ', validators=[DataRequired(), Length(max=60)])
+    genre_radio = RadioField('Genre: ', choices=[('1', 'Homme'), ('2', 'Femme'), ('3', 'Ananas')], validators=[DataRequired()])
+    demand_checkbox = MultiCheckboxField('Demande: ', choices=['Autre', 'Demande', 'Réparation'], validators=[MultiCheckboxAtLeastOne()])
+    comment = TextAreaField('Message: ', validators=[DataRequired()])
     submit = SubmitField("Soumettre")
 
 # Create Form Page
@@ -25,23 +41,14 @@ class NamerForm(FlaskForm):
 def formulaire_wtforms():
     name = None
     lastname = None
-    
-    country_list = []
-    countries = pycountry.countries
-    for country in countries:
-        country_list.append(country.name)
-
     form = NamerForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-        lastname = form.lastname.data
-        form.lastname.data = ''
-    return render_template("formulaire_wtforms.html",
-                           name = name,
-                           lastname = lastname,
-                           country = country_list,
-                           form = form,)
+    if name != None:
+        return render_template('thankyou.html')
+    else:
+        return render_template("formulaire_wtforms.html",
+                            name = name,
+                            lastname = lastname,
+                            form = form,)
 
 if __name__ == '__main__':
     app.run(debug=True)
