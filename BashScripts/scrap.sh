1c729bfd514e7c2f8231aefe83b6e7e29c0d1e1a
Max Brignoli <brignolimax1@gmail.com>
2023-06-12 16:53:31 +0200

    jun12-16:53

--- BashScripts/scrap.sh
+++ BashScripts/scrap.sh
@@ -2,4 +2,8 @@
 
 curl $1 > ./tmp_scrap
 
-cat ./tmp_scrap | grep '<h4 class="pull-right price">'
+description=$(cat ./tmp_scrap | grep '<p class="description">' | cut -d '>' -f2 | cut -d '<' -f1 | sed 's/\&quot;/\"/' | cut -d ',' -f 2-)
+
+title=$(cat ./tmp_scrap | grep 'class="title"' | grep -E -o 'title="[^"]*">' | cut -d '"' -f2 | sed 's/\&quot;/\"/')
+
+price=$(cat ./tmp_scrap | grep -E -o '\$[0-9]+(\.[0-9]{2})?')
