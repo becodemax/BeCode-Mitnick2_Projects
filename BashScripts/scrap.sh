a5c9d1560e763860520dd27164f01997731d5da9
Max Brignoli <brignolimax1@gmail.com>
2023-06-14 09:00:30 +0200

    jun14-09:00

--- BashScripts/scrap.sh
+++ BashScripts/scrap.sh
@@ -1,23 +1,10 @@
 
 #!/bin/sh
 
-
 html=$(curl -s "https://webscraper.io/test-sites/e-commerce/allinone/computers/laptops")
 
-# TESTS
+title=$(echo "$html" | sed -n 's/<a href="[^"]*" class="title" title="[^"]*">\([^<]*\)<\/a>/\1/p' | sed 's/\s//g')
+descriptions=$(echo "$html" | sed -n 's/<p class="description">\([^<]*\)<\/p>/\1/p' | sed 's/\s//g' |sed 's/&quot;/"/g')
+price=$(echo "$html" | sed -n 's/<h4 class="pull-right price">\([^<]*\)<\/h4>/\1/p' | sed 's/\s//g')
 
-# description=$(cat "$html" | grep '<p class="description">' | cut -d '>' -f2 | cut -d '<' -f1 | sed 's/\&quot;/\"/' | cut -d ',' -f 2-)
-# title=$(cat "$html" | grep 'class="title"' | grep -E -o 'title="[^"]*">' | cut -d '"' -f2 | sed 's/\&quot;/\"/')
-# price=$(cat "$html" | grep -E -o '\$[0-9]+(\.[0-9]{2})?')
-content=$(echo "$html" | sed -n '/<div class="caption">/,/<\/div>/p' | sed -E 's/<[^>]*>//g' | sed '/^\s*$/d')
-
-echo "$content"
-# echo "$title | $description | $price"
-
-
-
-# title=$(echo "$html" | sed -n 's/<a href="[^"]*" class="title" title="[^"]*">\([^<]*\)<\/a>/\1/p')
-# descriptions=$(echo "$html" | sed -n 's/<p class="description">\([^<]*\)<\/p>/\1/p' | sed 's/&quot;/"/g')
-# price=$(echo "$html" | sed -n 's/<h4 class="pull-right price">\([^<]*\)<\/h4>/\1/p')
-
-# echo "$title $descriptions $price" | sed -E 's/\s{2,}/ | /g'
+paste -d '|' <(echo "$title") <(echo "$descriptions") <(echo "$price") | sed 's/|/ | /g'
