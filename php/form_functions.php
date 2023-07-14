af6a4f24eef8e9f53d800411774dead0f048a727
Max <brignolimax1@gmail.com>
2023-07-14 18:22:19 +0200

    jul14-6:22PM

--- php/form_functions.php
+++ php/form_functions.php
@@ -1,5 +1,23 @@
 <?php
 
+// check if a variable is missing or compromised by comparing the size of post array with filter array
+function CheckArraysLength($arr, $filters) {
+    $arr_size = sizeof(array_intersect_key($arr, $filters));
+    if ( $arr_size === sizeof($filters) ) {
+        return true;
+    } elseif ( !in_array('recorded', $arr) ) { 
+        return false;
+    }
+}
+
+// check for any empty field except venue_address_2
+function CheckForEmpty($arr) {
+    if ( in_array('', $arr) && $arr['venue_address_2'] !== '' ) {
+        return true;
+    }
+}
+
+// compare post array values with filters array values (regex)
 function CheckWrongInput($arr, $filters) {
     $combined_arr = array_combine($arr, $filters);
     foreach ( $combined_arr as $input => $regx ) {
@@ -9,6 +27,19 @@ function CheckWrongInput($arr, $filters) {
     }
 }
 
+// check the uploaded file extension
+function CheckFileExtension($file_arr, $files_ext) {
+    $extension = $file_arr['type'];
+    if ( isset($extension) ) {
+        if ( in_array($extension, $files_ext) ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+// classic php sanitizing function just to be sure
 function SanitizeInput($arr) {
     foreach ( $arr as $key => $value ) {
         $value = trim($value);
@@ -19,34 +50,14 @@ function SanitizeInput($arr) {
     return $arr;
 }
 
-function CheckFileExtension($file_arr, $files_ext) {
-    $filename = $file_arr['name'];
-    $extenstion = $file_arr['type'];
-    if ( isset($filename) ) {
-        if ( in_array($extenstion, $files_ext) ) {
-            return true;
-        }
-    }
-}
-
-function CheckArraysLength($arr, $filters) {
-    if ( sizeof(array_intersect_key($filters, $arr)) === sizeof($filters) ) {
-        return true;
-    }
-}
-
-function CheckForEmpty($arr) {
-    if ( in_array('', $arr) ) {
-        return true;
-    }
-}
-
+// display error message over html
 function Error($err_message, $html) {
     $error = '<div style="background-color:red; color: white; padding: 10px; position: absolute; top: 0; left: 50%; width: 90%; text-align: center; transform: translate(-50%,0); z-index: 9999;">'.$err_message.'</div>';
     $html = str_replace('<body>', '<body>'.$error, $html);
     echo $html;
 }
 
+// final print of every values
 function PrintValues($arr) {
     foreach ( $arr as $key => $value ) {
         echo "<br>";
--- php/form_validate.php
+++ php/form_validate.php
@@ -6,19 +6,30 @@ require 'form_functions.php';
 $arr_post = $_POST;
 $file_arr = $_FILES['fileToUpload'];
 
-function ValidateForm($html, $arr_post, $file_arr, $filters, $files_ext) {
+// multiple checks on the inputs
+function ValidateForm($html, $arr_post, $file_arr, $filters, $files_ext) 
+{
+    // check every input var names and their integrity
     if ( CheckArraysLength($arr_post, $filters) === false ) {
         $err_message = "Something went wrong.";
         Error($err_message, $html);
+
+    // check for empty required fields
     } elseif ( CheckForEmpty($arr_post) === true ) {
-        $err_message = "One or more fields haven't been completed.";
+        $err_message = "Please complete all the fields.";
         Error($err_message, $html);
+
+    // check for any forbidden characters in fields (regex)
     } elseif ( CheckWrongInput($arr_post, $filters) === true ) {
-        $err_message = "Forbidden characters have been detected in one or more fields.";
+        $err_message = "Please complete all the fields properly.";
         Error($err_message, $html);
+
+    // check file extension (only png, jpeg or pdf)
     } elseif ( CheckFileExtension($file_arr, $files_ext) === false ) {
         $err_message = "Please only upload an image (png, jpeg or pdf).";
         Error($err_message, $html);
+
+    // if no errors above, sanitize all the inputs and print them (for now)
     } else {
         $sanitized_arr = SanitizeInput($arr_post);
         $file = $file_arr['name'];
